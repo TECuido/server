ca355610a04f3f59c6f7c1b81b69ee426df1b989
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-01-14 21:57:00 -0600

    Hotfix
    Regresamos la seguridad

--- routes/usuariodetalles.js
+++ routes/usuariodetalles.js
@@ -8,12 +8,14 @@ const UsuarioDetallesController = require("../controllers/usuariodetalles.js");
 const { createUsuarioDetallesSchema, getUsuarioDetallesSchema, putUsuarioDetallesSchema } = require("../schemas/usuarioDetalles.schema.js");
 
 
+
+
 const controller = new UsuarioDetallesController();
 
 // Ruteo de la parte de usuario
 router.get("/", isAuthenticated, controller.getAllUsuarioDetalles);
 router.get("/:id",  isAuthenticated, validatorHandler(getUsuarioDetallesSchema, "params"), controller.getUsuarioDetalles);
-router.post("/:id", controller.addUsuarioDetalles); 
+router.post("/:id", isAuthenticated, validatorHandler(createUsuarioDetallesSchema, "body"), validatorHandler(getUsuarioDetallesSchema,"params", controller.addUsuarioDetalles)); 
 router.put("/:id",isAuthenticated,  validatorHandler(putUsuarioDetallesSchema, "body"), validatorHandler(getUsuarioDetallesSchema, "params"),controller.updateUsuarioDetalles);
 
 // router.get(
